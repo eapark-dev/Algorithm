dd2120c2a01b0ed26f7098086bd200a8dd9ea979
eapark-dev <eapark5908@gmail.com>
2021-05-21 23:04:33 +0900

    프로그래머스 카카오 2021년 블라인드 테스트, 신규아이디 추천 알고리즘 문제

---
+++ programmers/level1/신규아이디추천.js
@@ -0,0 +1,38 @@
+function solution(new_id) {
+    var answer = '';
+    
+    answer = new_id.toLowerCase(); //소문자 치환
+    answer = answer.replace(/[^\w\d\-\_\.]/g, '')               //done
+                       .replace(/\.{2,}/g, '.')                     //done
+                       .replace(/^\.|\.$/g, '');      
+   
+    for (var i = answer.length - 1; i >= 0; i--) {
+        if (answer[i] === '.' && answer[i - 1] === '.') {
+            answer = answer.replace(answer[i], '');
+        }
+    }
+    
+    if(answer.substr(0,1) == "."){
+        answer = answer.substr(1);
+    };
+    
+    if(answer.substr(answer.substr-1,1) == ".") {
+         answer = answer.substr(answer.substr-1);
+    }
+    if(answer == "") {
+        answer = "a";
+    }
+    if(answer.length >= 15) {
+        answer = answer.substring(0,15);
+    }
+    if(answer.charAt(answer.length-1) == ".") {
+         answer = answer.slice(0,-1);
+    }
+   
+    if (answer.length < 3) {
+        for (var i = answer.length; i < 3; i++) {
+            answer += answer[answer.length - 1];
+        }
+    }
+    return answer;
+}
