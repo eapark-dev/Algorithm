e3b92c103c77c4b41ccab2f956fb374263ffd97a
eapark-dev <eapark5908@gmail.com>
2021-08-01 12:45:17 +0900

    프로그래머스. - 자릿수 더하기

---
+++ programmers/level1/plusNum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class plusNum {
+    public int solution(int n) {
+        int answer = 0;
+        // n값을 split 함수를 이용해 각가의 값 쪼갠다.
+        String[] tmp = Integer.toString(n).split("");
+        //반복문을 이용해 tmp에 들어있는 값들을 answer에 더한다.
+        for(int i=0; i<tmp.length;i++){
+            //string to int
+            answer += Integer.parseInt(tmp[i]);
+        }
+        
+        return answer;
+    }
+}
