fc172dd8ecccb9f5a26b68bfe74d3877af61eb89
eapark-dev <eapark5908@gmail.com>
2021-08-11 21:41:57 +0900

    프로그래머스 - 소수 찾기

---
+++ programmers/level1/findDecimal.java
@@ -0,0 +1,21 @@
+public class findDecimal {
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            for(int i=2; i<=n; i++){
+                boolean flag = true; //소수인지 판별하는 flag 추가
+                for(int j=2; j*j<=i; j++) {
+                    if(i % j == 0) {  //0으로 나눠지면 소수가 아니므로 flas = false , if문 나가기
+                        flag = false;
+                        break; 
+                    } 
+                }
+                
+                if(flag){
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
+}
