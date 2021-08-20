ccbb859764171eeb42906a8b7116eff7f0445c9d
Euna Park <80019618+eapark-dev@users.noreply.github.com>
2021-08-20 21:35:03 +0900

    프로그래머스 - 최소공약수와 최소공배수 구하기

---
+++ programmers/level1/gvdLcm.java
@@ -0,0 +1,23 @@
+public class gvdLcm {
+    class Solution {
+        public int[] solution(int n, int m) {
+            int[] answer = new int[2];
+            //최소 공배수 구하기 위해 임시로 넣어둠
+            int a = n; 
+            int b = m; 
+            //최대공약수 구하기
+            while(m > 0){
+                int tmp = m;
+                m = n % m;
+                n = tmp;
+            }
+            //최소 공배수 
+            answer[0] = n;
+            answer[1] = (a * b) / n;
+            
+            
+            return answer;
+        }
+        
+    }
+}
