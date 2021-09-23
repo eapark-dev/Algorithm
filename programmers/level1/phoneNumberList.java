d91fd3ee9720c0abd98700c52cd5e308eea67644
eapark-dev <eapark5908@gmail.com>
2021-09-23 21:39:54 +0900

    프로그래머스 - 전화번호 목록 알고리즘 , 테스트 통과 효율성 실패한 알고리즘

---
+++ programmers/level1/phoneNumberList.java
@@ -0,0 +1,18 @@
+public class phoneNumberList {
+    class Solution {
+        public boolean solution(String[] phone_book) {
+            boolean answer = true;
+            for(int i=0; i<phone_book.length; i++){
+                for(int j=i+1; j<phone_book.length; j++){
+                    if(phone_book[i].startsWith(phone_book[j])){
+                        answer = false;
+                    }
+                    if(phone_book[j].startsWith(phone_book[i])){
+                        answer = false;
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+}
