91fb338c25a0ce89af34e118169245926c5aced6
eapark-dev <eapark5908@gmail.com>
2021-09-25 22:12:39 +0900

    level1폴더에서 level2로 파일 이동

--- programmers/level2/phoneNumberList.java
+++ programmers/level2/phoneNumberList.java
@@ -4,6 +4,7 @@ public class phoneNumberList {
             boolean answer = true;
             for(int i=0; i<phone_book.length; i++){
                 for(int j=i+1; j<phone_book.length; j++){
+                    //startsWith 함수 사용하여 첫번째 값이 접두어인지 확인
                     if(phone_book[i].startsWith(phone_book[j])){
                         answer = false;
                     }
