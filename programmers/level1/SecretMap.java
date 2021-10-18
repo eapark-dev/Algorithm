79e7d1a552420745879dc0cbdea32091319b0ec3
eapark-dev <eapark5908@gmail.com>
2021-10-18 21:27:09 +0900

    프로그래머스 - 카카오 블라인드 문제 , 1차 비밀지도

---
+++ programmers/level1/SecretMap.java
@@ -0,0 +1,20 @@
+public class SecretMap {
+    class Solution {
+        public String[] solution(int n, int[] arr1, int[] arr2) {
+            String[] answer = new String[n];
+            
+            for(int i=0; i<arr1.length; i++){
+                answer[i] = String.format("%"+n+"d",Long.parseLong(Integer.toBinaryString(arr1[i]|arr2[i])));
+                
+                if(answer[i].length() < n){
+                    answer[i] = "0" + answer[i];
+                }
+                
+                answer[i] = answer[i].replaceAll("1", "#");
+                    answer[i] = answer[i].replaceAll("0", " ");
+            }
+            
+            return answer;
+        }
+    }
+}
