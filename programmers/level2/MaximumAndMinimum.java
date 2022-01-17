283b86ad7d44a900bc596d2a26b16fe8ddc2fc44
eapark-dev <eapark5908@gmail.com>
2022-01-17 22:25:11 +0900

    프로그래머스 - 최솟값과 최댓값 주석 추가

--- programmers/level2/MaximumAndMinimum.java
+++ programmers/level2/MaximumAndMinimum.java
@@ -4,15 +4,16 @@ public class MaximumAndMinimum {
     class Solution {
         public String solution(String s) {
             String answer = "";
-            String[] tmp = s.split(" ");
-            int[] arr = new int[tmp.length];
+            String[] tmp = s.split(" "); //문자열 split
+            int[] arr = new int[tmp.length]; //int 배열 만들기
             
-            for(int i=0; i<tmp.length; i++){
+            for(int i=0; i<tmp.length; i++){ //arr 배열에 담기
                 arr[i] = Integer.parseInt(tmp[i]);
             }
             
-            Arrays.sort(arr);
+            Arrays.sort(arr); //오름차순 정렬 
             
+            //맨 앞에 있는 값과 맨 뒤에 값 넣기
             answer += Integer.toString(arr[0]) + " " +Integer.toString(arr[arr.length-1]);
             
             return answer;
