b90a33e4aa6f66015c9fd2ecd252418c08487d1c
eapark-dev <eapark5908@gmail.com>
2022-02-26 20:43:50 +0900

    백준 알고리즘 - 별 찍기 - 1

---
+++ baekjoon/Algorithm_2438.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Algorithm_2438 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        for(int i=0; i<cnt; i++){
+            String star = "*";
+            int j = 1;
+            while(j<=i){
+                star += "*";
+                j++;
+            }
+            System.out.println(star);
+        }
+    }
+}
