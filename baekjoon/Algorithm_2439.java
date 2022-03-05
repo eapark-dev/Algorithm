58858d134733acc6bd77f937bbb5b4d264233279
eapark-dev <eapark5908@gmail.com>
2022-03-05 21:30:59 +0900

    백준 알고리즘 - 2439번 문제 런타임 해결

--- baekjoon/Algorithm_2439.java
+++ baekjoon/Algorithm_2439.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 public class Algorithm_2439 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int num = sc.nextInt();
-        for(int i = 1; i<=num; i++){
-            for(int j=1; j<=num-i; j++){
-                System.err.print(" ");
-            }
-            for(int k=1; k<=i; k++){
-                System.err.print("*");
-            }
-            System.out.println();
-        }
+        sc.close();
+
+
+		for (int i = 1; i <= num; i++) {
+			for (int j = 1; j <= num - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k < i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
     }
 }
