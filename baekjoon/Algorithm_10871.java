56447ee26c7b41a52be040766e6d4c948ed8de8d
eapark-dev <eapark5908@gmail.com>
2022-03-06 19:26:56 +0900

    백준 알고리즘 - 10871번 문제 X보다 작은 수

---
+++ baekjoon/Algorithm_10871.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Algorithm_10871 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int num = sc.nextInt();
+        String str = "";
+        for(int i=1; i<=len; i++){
+            int a = sc.nextInt();
+            if(a < num){
+                str += a + " ";
+            }
+        }
+
+        System.out.println(str.strip());
+    }
+}
