aaf66610b5ef546e3db953f9005f01aa5adfc3f0
eapark-dev <eapark5908@gmail.com>
2022-03-07 20:40:54 +0900

    백준 알고리즘 - 2525번 문제 오븐시계

---
+++ baekjoon/Algorithm_2525.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Algorithm_2525 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int hour = sc.nextInt();
+        int min = sc.nextInt();
+        int addMin = sc.nextInt();
+
+        int sumMin = min + addMin;
+
+        //60이상인 경우 
+        if(sumMin >= 60){
+            //총 값이 60보다 클 경우 60분씩 계속 차감 
+            while(sumMin >= 60){
+                sumMin -= 60;
+                hour++;
+                //24를 넘어가면 0으로 초기화
+                if(hour >= 24) {
+                    hour=0;
+                }
+            }
+        }
+
+        System.out.println(hour + " " + sumMin);
+
+    }
+}
