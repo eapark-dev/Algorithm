8d3c9f9473a564299c17f29fda774aa12a50ea1e
eapark-dev <eapark5908@gmail.com>
2022-06-24 20:28:10 +0900

    해커랭크 - extraLongFactorials 알고리즘

---
+++ hackerrank/extraLongFactorialsClass.cs
@@ -0,0 +1,45 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Numerics;
+
+class extraLongFactorialsClass
+{
+
+    /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void extraLongFactorials(int n)
+    {
+        BigInteger sum = 1;
+        for(BigInteger i = n; i!=1; i--){
+            sum *= i;
+        }
+        
+        Console.WriteLine(sum);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.extraLongFactorials(n);
+    }
+}
