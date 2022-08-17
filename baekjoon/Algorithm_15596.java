652fda771189a95ff82eab41f3d364478ac7bcd3
eapark-dev <eapark5908@gmail.com>
2022-08-17 20:50:12 +0900

    백준 알고리즘 - 정수 N개의 합 15596번 문제

---
+++ baekjoon/Algorithm_15596.java
@@ -0,0 +1,10 @@
+public class Algorithm_15596 {
+    public long Sum(int[] arr) {
+        long sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        return sum;
+    }
+}
