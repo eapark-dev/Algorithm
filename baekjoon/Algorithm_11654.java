b7faaa6148d7b411c5436ec13c269d94708dd7a6
eapark-dev <eapark5908@gmail.com>
2022-08-20 17:16:23 +0900

    백준 알고리즘 - 아스키 코드 11654번 문제

---
+++ baekjoon/Algorithm_11654.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Algorithm_11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ascll = sc.nextLine().charAt(0);
+        System.out.println((int) ascll);
+    }
+}
